fd1424f5fb482e56c609f6f00accc1d4207bcbe7
oandreazini <55554433+oandreazini@users.noreply.github.com>
2022-08-18 19:09:12 +0200

    Ex3-deploy-heroku

---
+++ src/test/java/mainApp/T263ApplicationTests.java
@@ -0,0 +1,13 @@
+package mainApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T263ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
